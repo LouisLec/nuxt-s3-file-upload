24e122f535aa791c0d791ed90576a281055a2dac
LouisLec <louis.l@obole-digitale.fr>
2020-01-02 23:50:47 +0100

    feat(file upload): file input form, upload methods
    TODO: cvheck how to retrieve

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,66 @@
 <template>
-  
+  <v-container>
+    <v-file-input v-model="myFile" @change="onFileChange">
+      File to upload to S3
+    </v-file-input>
+    <v-btn @click="logger()">check buckets</v-btn>
+    <v-btn @click="upload()">upload</v-btn>
+    <v-btn @click="fetch()">fetch</v-btn>
+  <v-container>
+    {{returnedData}}
+    {{returnedURL}}
+  </v-container>
+    <img :scr="returnedURL"/>
+  </v-container>
 </template>
 
 <script>
+  export default {
+    data () {
+      return {
+      myFile: null,
+      returnedData: null,
+      returnedURL: null,
+      }
+    },
+    methods: {
+      logger () {
+        this.$s3.listBuckets(function(err, res) {
+          console.log(err||res)
+        });
+      },
+      upload () {
+        const res = this.$s3.getSignedUrl('putObject', {
+          Body: this.myFile,
+          Key: this.myFile.name,
+          Bucket: "sopolyglot-dev"
+        }, (err, data) => {
+          if (err) {
+              throw err;
+          }
+          console.log(data);
+          this.returnedData = data
+        })
+      },
+      fetch () {
+        this.$s3.getSignedUrl('getObject', {
+          Key: "FOHDL5PYLAI2PG9IX9VA",
+          Bucket: "sopolyglot-dev"
+        }, (err, data) => {
+          if (err) {
+              throw err;
+          }
+          console.log(data);
+          this.returnedURL = data;
+        });
+      },
+      onFileChange() {
+        console.log(this.myFile);
+      },
+      // this.$s3.listBuckets(function(err, res) {
+      //   console.log(err||res)
+      // });
+    }
 
+  }
 </script>
